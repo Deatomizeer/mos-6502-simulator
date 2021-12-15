6284436cf646d72961600d93f51dc7b6c678d071
Deatomizeer <56254654+Deatomizeer@users.noreply.github.com>
2021-12-15 21:13:25 +0100

    Created memory class
    On start, the registers, RAM and flags are now initialized. However, it's not visually represented on the screen yet.

--- Assets/MemoryAndRegisters.cs
+++ Assets/MemoryAndRegisters.cs
@@ -1,24 +1,31 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// This class 
+// This class contains variables related to the simulation's state: registers, flags and memory.
 public class MemoryAndRegisters : MonoBehaviour
 {
-    // Processor registers
-    Dictionary<string, int> register = new Dictionary<string, int>();
+    // Processor registers.
+    public Dictionary<string, int> register = new Dictionary<string, int>();
+    // Random Access Memory.
+    int memorySize = 200;
+    public sbyte[] memory;
+    // Flags register, with each bit corresponding to a specific flag: NV-BDIZC.
+    public sbyte flags;
+
     void Start()
     {
-        // Initialize the registers
+        // Initialize the accumulator, X, Y, and program count registers.
         register.Add("AC", 0);
         register.Add("X", 0);
         register.Add("Y", 0);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        register.Add("PC", 0);
+        // Initialize the RAM (200 address spaces for now).
+        memory = new sbyte[memorySize];
+        for(int i=0; i<memorySize; i++)
+        {
+            memory[i] = 0;
+        }
+        // Initialize the flags.
+        flags = 0b00110000;
     }
 }
