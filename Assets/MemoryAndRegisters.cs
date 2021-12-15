e6f14326b3ef2c471b2942b7904ce537699a5cf3
Deatomizeer <56254654+Deatomizeer@users.noreply.github.com>
2021-12-16 00:45:32 +0100

    Added basic register and flag code
    On start, the initial values in all registers and flags are displayed through text. The numbers are static for now, and cannot be altered by any means.

--- Assets/MemoryAndRegisters.cs
+++ Assets/MemoryAndRegisters.cs
@@ -1,13 +1,23 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 // This class contains variables related to the simulation's state: registers, flags and memory.
 public class MemoryAndRegisters : MonoBehaviour
 {
+    // References to display text for all the values.
+    public Text acText;
+    public Text xText;
+    public Text yText;
+    public Text pcText;
+    public Text flagsText;
+
     // Processor registers.
     public Dictionary<string, int> register = new Dictionary<string, int>();
     // Random Access Memory.
-    int memorySize = 200;
+    int memorySize = 100;
     public sbyte[] memory;
     // Flags register, with each bit corresponding to a specific flag: NV-BDIZC.
     public sbyte flags;
@@ -27,5 +37,18 @@ public class MemoryAndRegisters : MonoBehaviour
         }
         // Initialize the flags.
         flags = 0b00110000;
+
+        // Set the default text for all data.
+        acText.text = "AC: " + register["AC"];
+        xText.text = "X: " + register["X"];
+        yText.text = "Y: " + register["Y"];
+        pcText.text = "PC: " + register["PC"];
+
+        // Convert flags to binary format, filling leading zeroes as needed.
+        string flagsBinary = Convert.ToString(flags, 2);
+        flagsBinary = flagsBinary.PadLeft(8, '0');   // What a nice function!
+        flagsText.text = "NV-BDIZC\n" + flagsBinary;
+        
+
     }
 }
